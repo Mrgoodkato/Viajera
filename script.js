be79ee11147b96137363cfeb686999545cd8c8da
Mrgoodkato <mrgoodkato@gmail.com>
2022-12-19 13:12:27 -0500

    Test for mobile 2

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ console.log(intro);
 var index = 0;
 
 $(function() {
-    $('#terminal').terminal(function() {
+    $('html').terminal(function() {
 
         this.echo(intro.data[index].text);
         index++;
