630e0d7797027ca32be913d91a1e9a914175c6aa
Mrgoodkato <mrgoodkato@gmail.com>
2022-12-22 14:03:01 -0500

    Antoher change

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const index = {
 const inputField = document.getElementsByClassName('clipboard');
 
 $('#terminal').terminal(function() {
-    if(index.level == 0) introGame(dummyIntro, index, this);
+    if(index.level == 0) introGame(intro, index, this);
     else if(index.level == 1) level1(level_1, index, this);
 }, 
 {
